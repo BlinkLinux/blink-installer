6b80553c2fe96c00ba352ab418bd777bacad3697
Xu Shaohua <shaohua@biofan.org>
2022-08-09 17:58:41 +0800

    resources: Add misc/ header file

---
+++ resources/misc/misc.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef BLINK_INSTALLER_RESOURCES_MISC_MISC_H_
+#define BLINK_INSTALLER_RESOURCES_MISC_MISC_H_
+
+namespace installer {
+
+constexpr const char kMiscLanguages[] = ":/misc/languages.json";
+constexpr const char kMiscReservedUsernames[] = ":/misc/reserved_usernames";
+constexpr const char kMiscTimezoneAlias[] = ":/misc/timezone_alias";
+constexpr const char kMiscTzBackward[] = ":/misc/tz_backward";
+
+}  // namespace installer
+
+#endif //BLINK_INSTALLER_RESOURCES_MISC_MISC_H_
