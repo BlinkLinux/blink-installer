98bfb503aed1bf4aa5a5cf5792c726706d4567d1
Xu Shaohua <shaohua@biofan.org>
2022-08-09 18:18:19 +0800

    slide: Add index header file

---
+++ resources/slide/slide.h
@@ -0,0 +1,80 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef BLINK_INSTALLER_RESOURCES_SLIDE_SLIDE_H_
+#define BLINK_INSTALLER_RESOURCES_SLIDE_SLIDE_H_
+
+namespace installer {
+
+// slide/zh_CN/
+constexpr const char* kSlideListZhCN[] = {
+  ":/slide/zh_CN/01-deepin-file-manager.png",
+  ":/slide/zh_CN/02-deepin-app-store.png",
+  ":/slide/zh_CN/03-deepin-movie.png",
+  ":/slide/zh_CN/04-deepin-image-viewer.png",
+  ":/slide/zh_CN/05-deepin-screenshot.png",
+  ":/slide/zh_CN/06-deepin-music.png",
+  ":/slide/zh_CN/07-deepin-terminal.png",
+  ":/slide/zh_CN/08-deepin-cloud-print.png",
+  ":/slide/zh_CN/09-deepin-utility.png",
+  ":/slide/zh_CN/10-deepin-remote-assistance.png",
+  ":/slide/zh_CN/11-deepin-community.png",
+  ":/slide/zh_CN/12-deepin-dock.png",
+  ":/slide/zh_CN/13-deepin-control-center.png",
+  ":/slide/zh_CN/14-deepin-kernel.png",
+  ":/slide/zh_CN/15-animation.png",
+  ":/slide/zh_CN/16-wps-youdao-dict.png",
+  ":/slide/zh_CN/18-netease-cloud-music.png",
+  ":/slide/zh_CN/19-deepin-screen-recorder.png",
+  ":/slide/zh_CN/20-wps.png",
+  ":/slide/zh_CN/21-youdao-dict.png",
+};
+
+// slide/zh_TW/
+constexpr const char* kSlideListZhTW[] = {
+  ":/slide/zh_TW/01-deepin-file-manager.png",
+  ":/slide/zh_TW/02-deepin-app-store.png",
+  ":/slide/zh_TW/03-deepin-movie.png",
+  ":/slide/zh_TW/04-deepin-image-viewer.png",
+  ":/slide/zh_TW/05-deepin-screenshot.png",
+  ":/slide/zh_TW/06-deepin-music.png",
+  ":/slide/zh_TW/07-deepin-terminal.png",
+  ":/slide/zh_TW/08-deepin-cloud-print.png",
+  ":/slide/zh_TW/09-deepin-utility.png",
+  ":/slide/zh_TW/10-deepin-remote-assistance.png",
+  ":/slide/zh_TW/11-deepin-community.png",
+  ":/slide/zh_TW/12-deepin-dock.png",
+  ":/slide/zh_TW/13-deepin-control-center.png",
+  ":/slide/zh_TW/14-deepin-kernel.png",
+  ":/slide/zh_TW/15-animation.png",
+  ":/slide/zh_TW/16-wps.png",
+  ":/slide/zh_TW/17-deepin-screen-recorder.png",
+};
+
+// slide/default/
+constexpr const char* kSlideListDefault[] = {
+  ":/slide/default/01-deepin-file-manager.png",
+  ":/slide/default/02-deepin-app-store.png",
+  ":/slide/default/03-deepin-movie.png",
+  ":/slide/default/04-deepin-image-viewer.png",
+  ":/slide/default/05-deepin-screenshot.png",
+  ":/slide/default/06-deepin-music.png",
+  ":/slide/default/07-deepin-terminal.png",
+  ":/slide/default/08-deepin-cloud-print.png",
+  ":/slide/default/09-deepin-utility.png",
+  ":/slide/default/10-deepin-remote-assistance.png",
+  ":/slide/default/11-deepin-community.png",
+  ":/slide/default/12-deepin-dock.png",
+  ":/slide/default/13-deepin-control-center.png",
+  ":/slide/default/14-deepin-kernel.png",
+  ":/slide/default/15-animation.png",
+  ":/slide/default/16-wps.png",
+  ":/slide/default/17-skype.png",
+  ":/slide/default/18-spotify.png",
+  ":/slide/default/19-deepin-screen-recorder.png",
+};
+
+}  // namespace installer
+
+#endif  // BLINK_INSTALLER_RESOURCES_SLIDE_SLIDE_H_
