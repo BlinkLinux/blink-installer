c246336a6a439dd8c7c06ccb2e8073724edca283
Xu Shaohua <shaohua@biofan.org>
2022-08-13 17:11:20 +0800

    app: Add timezone frame test

--- src/app/timezone_frame_test.cpp
+++ src/app/timezone_frame_test.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ * Copyright (C) 2022 Xu Shaohua <shaohua@biofan.org>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,5 +30,5 @@ int main(int argc, char* argv[]) {
   QObject::connect(&frame, &installer::TimezoneFrame::finished,
                    &app, &QApplication::quit);
 
-  return app.exec();
+  return QApplication::exec();
 }
